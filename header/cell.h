5c73414d982bafb4289b3ceea7423eede0e2ec6c
Charles Yaiche <charles.yaiche@epita.fr>
2022-10-19 10:41:00 +0200

    [IMPL] add empty core function for cell struct

--- header/cell.h
+++ header/cell.h
@@ -5,4 +5,30 @@
 #ifndef ESGI_CASSEBRICK_CELL_H
 #define ESGI_CASSEBRICK_CELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "player.h"
+#include "bomb.h"
+
+enum kind_cell {
+    PLAYER,
+    BOMB,
+    OBJECT,
+    WALL,
+    VOID
+};
+
+typedef struct {
+    char c;
+    int is_empty; // 1 if empty, 0 if not
+    int is_destructible; // 1 if destructible (wall)
+    enum kind_cell kind_cell;
+    void *content; // can be a Player*, a Bomb* or an Object*
+} Cell;
+
+Cell *build_cell();
+void free_cell(Cell *cell);
+int update_cell(void *new_content);
+char get_char_cell(Cell *cell);
+
 #endif //ESGI_CASSEBRICK_CELL_H
--- src/cell.c
+++ src/cell.c
@@ -3,3 +3,20 @@
 //
 
 #include "../header/cell.h"
+
+
+Cell *build_cell(){
+    //TODO
+    return NULL;
+}
+void free_cell(Cell *cell){
+    //TODO
+}
+int update_cell(void *new_content){
+    //TODO
+    return 0;
+}
+char get_char_cell(Cell *cell){
+    //TODO
+    return '0';
+}

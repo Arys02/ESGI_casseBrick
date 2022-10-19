131791effc5ca8199c2766460fe7a9a74fdba07e
Charles Yaiche <charles.yaiche@epita.fr>
2022-10-19 12:48:57 +0200

    [IMPL] struct for board

--- header/board.h
+++ header/board.h
@@ -5,4 +5,20 @@
 #ifndef ESGI_CASSEBRICK_BOARD_H
 #define ESGI_CASSEBRICK_BOARD_H
 
+#include "object.h"
+#include "player.h"
+#include "bomb.h"
+#include "cell.h"
+
+typedef struct {
+    int width;
+    int length;
+    Cell **board;
+
+    //let include the players in board for now
+    int nb_player;
+    Player **players;
+}
+Board;
+
 #endif //ESGI_CASSEBRICK_BOARD_H

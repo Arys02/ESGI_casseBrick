6488033cff6d9fc789129583a7f221c353b80abb
Charles Yaiche <charles.yaiche@epita.fr>
2022-10-19 10:43:30 +0200

    [ADD] empty object.{c|h}

---
+++ header/object.h
@@ -0,0 +1,8 @@
+//
+// Created by Arys on 19/10/2022.
+//
+
+#ifndef ESGI_CASSEBRICK_OBJECT_H
+#define ESGI_CASSEBRICK_OBJECT_H
+
+#endif //ESGI_CASSEBRICK_OBJECT_H
---
+++ src/object.c
@@ -0,0 +1,5 @@
+//
+// Created by Arys on 19/10/2022.
+//
+
+#include "../header/object.h"

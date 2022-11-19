218402f68307a26e86d881930fa0387b8c284afb
Charles Yaiche <charles.yaiche@epita.fr>
2022-11-19 10:28:55 +0100

    [IMPL] moving function for player

--- src/player_action.c
+++ src/player_action.c
@@ -9,15 +9,21 @@ void drop_bomb_player(Player *p, Board *b){
     //TODO
 }
 
+
+
 void player_move_up(Player *p, Board *b){
-    //TODO
+    switch_cell(b, p->pos_x, p->pos_y, p->pos_x - 1, p->pos_y);
+    update_player_pos(p, p->pos_x - 1, p->pos_y);
 }
 void player_move_down(Player *p, Board *b){
-   //TODO
+    switch_cell(b, p->pos_x, p->pos_y, p->pos_x + 1, p->pos_y);
+    update_player_pos(p, p->pos_x + 1, p->pos_y);
 }
 void player_move_right(Player *p, Board *b){
-
+    switch_cell(b, p->pos_x, p->pos_y, p->pos_x, p->pos_y + 1);
+    update_player_pos(p, p->pos_x, p->pos_y + 1);
 }
 void player_move_left(Player *p, Board *b){
-   //TODO
+    switch_cell(b, p->pos_x, p->pos_y, p->pos_x, p->pos_y - 1);
+    update_player_pos(p, p->pos_x, p->pos_y - 1);
 }
--- test_src/test_player_action.c
+++ test_src/test_player_action.c
@@ -4,7 +4,9 @@
 
 #include <stdio.h>
 #include <conio.h>
+#include <pthread_time.h>
 #include <unistd.h>
+//#include <windows.h>
 #include "../header/player_action.h"
 
 int main() {
@@ -21,9 +23,23 @@ int main() {
             }
              if (frappe == 'p'){
                  printf("\n p pressed \n");
+                 player_move_up(b->players[0], b);
+            }
+              if (frappe == 'e'){
+                 printf("\n e pressed \n");
+                 player_move_right(b->players[0], b);
+            }
+              if (frappe == 'u'){
+                 printf("\n u pressed \n");
+                 player_move_left(b->players[0], b);
+            }
+              if (frappe == 'i'){
+                 printf("\n i pressed \n");
+                 player_move_down(b->players[0], b);
             }
         }
-        sleep(1);
+        //Sleep(500);
+        usleep(500 * 1000);
 
         printf("\n\n");
 
